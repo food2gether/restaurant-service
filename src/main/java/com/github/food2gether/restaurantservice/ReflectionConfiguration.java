b428547181d3e3b49a70bf09889d2e15936dcd1d
Marfien <contact@marfien.dev>
2025-03-02 19:03:25 +0100

    fix: reflection config

--- src/main/java/com/github/food2gether/restaurantservice/ReflectionConfiguration.java
+++ src/main/java/com/github/food2gether/restaurantservice/ReflectionConfiguration.java
@@ -1,11 +1,16 @@
 package com.github.food2gether.restaurantservice;
 
+import com.github.food2gether.shared.model.MenuItem;
+import com.github.food2gether.shared.model.Restaurant;
 import com.github.food2gether.shared.response.DataAPIResponse;
 import com.github.food2gether.shared.response.ErrorAPIResponse;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
 @RegisterForReflection(
     targets = {
+        MenuItem.DTO.class,
+        Restaurant.DTO.class,
+        Restaurant.Address.class,
         DataAPIResponse.class,
         ErrorAPIResponse.class
     }
